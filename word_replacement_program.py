ca969e8765e1614d06398587df74e0b59c18d0ca
Maxwell Muthui Mwangi(Justmwangii) <muthuimaxwell67@gmail.com>
2024-02-11 05:24:28 +0300

    new files

---
+++ word_replacement_program.py
@@ -0,0 +1,15 @@
+def replace_word():
+
+         
+
+         str = "Hi guys, I am Max, a Software Engineer at Ballershopke"
+         word_to_replace= input("Enter the word to replace: ")
+         word_replacement = input("Enter the word replacement: ")
+         print(str.replace(word_to_replace, word_replacement))
+
+
+def Main():
+
+    replace_word()
+
+Main()    
